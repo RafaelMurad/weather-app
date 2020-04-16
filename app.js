b69b11e9c2e84b90ca3dd2c555703d0f5d6c0772
Rfl <rafaeldeoliveiragonsalesmurad@rafaels-mbp.home>
2020-04-16 21:58:52 +0100

    installing request package and making my first http request

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('2')
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=84da104444694bbe3d2aef402537c5b4&query=37.8267,-122.4233'
+
+request({url: url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
