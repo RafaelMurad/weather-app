6d6b090568709b7c72e22914184aeb235fbf0bc0
RafaelMurad <rflmurad@gmail.com>
2020-04-18 13:57:16 +0100

    logs api calls

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=84da104444694bbe3d2aef402537c5b4&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=3acb472106515c1b14faf11f38d2bc90&query=37.8267,-122.4233'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    //console.log(response.body.current)
+    const temperature = response.body.current.temperature
+    const feelslike = response.body.current.feelslike
+
+    console.log(response.body.current.weather_descriptions[0] + ": it's currently " + temperature + " degrees outside, but it feels like " + feelslike)
 })
+
+//it's currently x degrees outside, but it feels like y. 
