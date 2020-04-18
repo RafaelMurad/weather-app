b8b4fa0425055a672c8c87f3b345c008401f4b3e
RafaelMurad <rflmurad@gmail.com>
2020-04-18 15:12:56 +0100

    handle some errors

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const request = require('request')
+const request = require("request");
 
-const url = 'http://api.weatherstack.com/current?access_key=3acb472106515c1b14faf11f38d2bc90&query=37.8267,-122.4233'
+const url =
+  "http://api.weatherstack.com/current?access_key=3acb472106515c1b14faf11f38d2bc90&query=37.8267,-122.4233";
 
-request({url: url, json: true}, (error, response) => {
-    //console.log(response.body.current)
-    const temperature = response.body.current.temperature
-    const feelslike = response.body.current.feelslike
+request({ url: url, json: true }, (error, response) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-    console.log(response.body.current.weather_descriptions[0] + ": it's currently " + temperature + " degrees outside, but it feels like " + feelslike)
-})
+  if (response.body && response.body.current) {
+    const temperature = response.body.current.temperature;
+    const feelslike = response.body.current.feelslike;
 
-//it's currently x degrees outside, but it feels like y. 
+    console.log(
+      `${response.body.current.weather_descriptions[0]}: it is currently ${temperature} degrees outside, but it feels like ${feelslike}`
+    );
+  } else {
+    console.log(`No data found on URL: ${url}`);
+  }
+});
