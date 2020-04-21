36ae8433732ddc15c1fb0cba6e2029cf18305aaa
RafaelMurad <rflmurad@gmail.com>
2020-04-21 21:46:00 +0100

    consuming geocode http call data

--- app.js
+++ app.js
@@ -16,8 +16,24 @@ request({ url: url, json: true }, (error, response) => {
     console.log(
       `${response.body.current.weather_descriptions[0]}: it is currently ${temperature} degrees outside, but it feels like ${feelslike}`
     );
-    return
-  } 
-    console.log(`No data found on URL: ${url}`);
-  
+    return;
+  }
+  console.log(`No data found on URL: ${url}`);
+});
+
+// Geocoding API
+
+const geocoding =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmZsbXVyYWQiLCJhIjoiY2s5YWJ1eTlhMGJnZDNlbnJ4am10ZTIzeCJ9.NNhDMrqRlbbyRz71ITOoeQ&limit=1";
+
+request({ url: geocoding, json: true }, (error, response) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+
+  console.log(`The latitude/longitude is: ${latitude} , ${longitude}`);
 });
